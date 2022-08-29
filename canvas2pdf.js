cfd9c00ac3cac1b7b7e6d1d35ffc3efa82b79b17
Amy Jo <amyjojjm2001@gmail.com>
2022-08-29 12:51:39 -0700

    modify blob require

--- canvas2pdf.js
+++ canvas2pdf.js
@@ -11,7 +11,7 @@
  *  Copyright (c) 2017 Joshua Gould
  */
 const PDFDocument = require("./lib/pdfkit.standalone");
-const blobStream = require("./lib/blob-stream");
+const blobStream = require("blob-stream");
 
 (function (global) {
   "use strict";
